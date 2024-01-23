7f12031ef49567d3b6278c68e43512b583c320e1
nekagit <nenad99@hotmail.de>
2024-01-24 00:14:21 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,146 @@
+
+<script setup lang="ts">
+import {
+Menubar,
+MenubarCheckboxItem,
+MenubarContent,
+MenubarItem,
+MenubarMenu,
+MenubarRadioGroup,
+MenubarRadioItem,
+MenubarSeparator,
+MenubarShortcut,
+MenubarSub,
+MenubarSubContent,
+MenubarSubTrigger,
+MenubarTrigger
+} from '@/components/ui/menubar';
+
+import { RouterView } from "vue-router";
+</script>
+
+
+<template>
+  <div class="h-screen flex flex-col justify-between " style="font-family:NM;">
+    <RouterView />
+  
+  <Menubar class="menubar">
+    <MenubarMenu>
+      <MenubarTrigger>File</MenubarTrigger>
+      <MenubarContent>
+        <MenubarItem>
+          New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem>
+          New Window <MenubarShortcut>⌘N</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem disabled>
+          New Incognito Window
+        </MenubarItem>
+        <MenubarSeparator />
+        <MenubarSub>
+          <MenubarSubTrigger>Share</MenubarSubTrigger>
+          <MenubarSubContent>
+            <MenubarItem>Email link</MenubarItem>
+            <MenubarItem>Messages</MenubarItem>
+            <MenubarItem>Notes</MenubarItem>
+          </MenubarSubContent>
+        </MenubarSub>
+        <MenubarSeparator />
+        <MenubarItem>
+          Print... <MenubarShortcut>⌘P</MenubarShortcut>
+        </MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+    <MenubarMenu>
+      <MenubarTrigger>Edit</MenubarTrigger>
+      <MenubarContent>
+        <MenubarItem>
+          Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem>
+          Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+        </MenubarItem>
+        <MenubarSeparator />
+        <MenubarSub>
+          <MenubarSubTrigger>Find</MenubarSubTrigger>
+          <MenubarSubContent>
+            <MenubarItem>Search the web</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Find...</MenubarItem>
+            <MenubarItem>Find Next</MenubarItem>
+            <MenubarItem>Find Previous</MenubarItem>
+          </MenubarSubContent>
+        </MenubarSub>
+        <MenubarSeparator />
+        <MenubarItem>Cut</MenubarItem>
+        <MenubarItem>Copy</MenubarItem>
+        <MenubarItem>Paste</MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+    <MenubarMenu>
+      <MenubarTrigger>View</MenubarTrigger>
+      <MenubarContent>
+        <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
+        <MenubarCheckboxItem checked>
+          Always Show Full URLs
+        </MenubarCheckboxItem>
+        <MenubarSeparator />
+        <MenubarItem inset>
+          Reload <MenubarShortcut>⌘R</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem disabled inset>
+          Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+        </MenubarItem>
+        <MenubarSeparator />
+        <MenubarItem inset>
+          Toggle Fullscreen
+        </MenubarItem>
+        <MenubarSeparator />
+        <MenubarItem inset>
+          Hide Sidebar
+        </MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+    <MenubarMenu>
+      <MenubarTrigger>Profiles</MenubarTrigger>
+      <MenubarContent>
+        <MenubarRadioGroup value="benoit">
+          <MenubarRadioItem value="andy">
+            Andy
+          </MenubarRadioItem>
+          <MenubarRadioItem value="benoit">
+            Benoit
+          </MenubarRadioItem>
+          <MenubarRadioItem value="Luis">
+            Luis
+          </MenubarRadioItem>
+        </MenubarRadioGroup>
+        <MenubarSeparator />
+        <MenubarItem inset>
+          Edit...
+        </MenubarItem>
+        <MenubarSeparator />
+        <MenubarItem inset>
+          Add Profile...
+        </MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+  </Menubar>
+  </div>
+
+</template>
+
+<style scoped>
+
+.menubar {
+ width: 200px; /* Twice the height to create a half circle */
+      height: 100px;
+  margin: 0 auto;
+        border-radius: 50% 50% 0 0; /* 50% for top-left, top-right, 0 for bottom-left and bottom-right */
+border-bottom:none ;
+border-left:none ;
+border-right:none ;
+padding: 0vh;
+}
+</style>
---
+++ src/components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+
+interface SkeletonProps {
+  class?: string
+}
+
+const props = defineProps<SkeletonProps>()
+</script>
+
+<template>
+    <div :class="cn('animate-pulse rounded-md bg-secondary', props.class)" >
+  <div class="logo text-sky-400">
+      
+      RY
+    </div>
+    </div>
+</template>
+<style scoped>
+.logo{
+    font-size: 64pt;
+    font-family:"NM";
+}
+</style>
